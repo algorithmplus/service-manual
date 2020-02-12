535d120801860494b7c660ce931fb55bf1b1ae67
Daniel Burnley <dan@danops.uk>
2020-02-12 18:26:44 +0000

    Recreate rails app without activerecord (+ rails 6)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,8 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
 end
