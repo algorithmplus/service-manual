daacc8a1237363a882c67295c46f9dc1329d2fc3
Marcin Cichon <slashrealm@gmail.com>
2020-03-25 13:40:16 +0000

    Replace name attr with id for H2 anchors

--- app/services/contentful_renderers/heading_two_renderer.rb
+++ app/services/contentful_renderers/heading_two_renderer.rb
@@ -1,11 +1,11 @@
 require 'rich_text_renderer'
 
 module ContentfulRenderers
-  # H1 node renderer.
+  # H2 node renderer.
   class HeadingTwoRenderer < RichTextRenderer::BaseBlockRenderer
     def render(node)
       hash = node['content'][0]['value'].hash
-      "<h2 class = 'jonny'><a name = '#{hash}'>#{render_content(node)}</a></h2>"
+      "<h2><a id='#{hash}'>#{render_content(node)}</a></h2>"
     end
   end
 end
