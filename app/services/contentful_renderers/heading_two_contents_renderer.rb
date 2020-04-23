f8ea7e4ae22a1f39a4790875de95c31eb7effda7
Marcin Cichon <slashrealm@gmail.com>
2020-04-23 16:32:26 +0100

    Fix broken lhs nav links

--- app/services/contentful_renderers/heading_two_contents_renderer.rb
+++ app/services/contentful_renderers/heading_two_contents_renderer.rb
@@ -4,8 +4,14 @@ module ContentfulRenderers
   # H1 node renderer.
   class HeadingTwoContentsRenderer < RichTextRenderer::BaseBlockRenderer
     def render(node)
-      hash = node['content'][0]['value'].hash
-      "<li class='govuk-body-s'><a class='govuk-link' href='##{hash}'>#{render_content(node)}</a></li>"
+      whole_heading = ''
+
+      node['content'].each do |content|
+        whole_heading += content['value']
+      end
+
+      hash = whole_heading.parameterize
+      "<li class='govuk-body-s'><a class='govuk-link' href='##{hash}'>#{whole_heading}</a></li>"
     end
   end
 end
--- app/services/contentful_renderers/heading_two_renderer.rb
+++ app/services/contentful_renderers/heading_two_renderer.rb
@@ -4,8 +4,14 @@ module ContentfulRenderers
   # H2 node renderer.
   class HeadingTwoRenderer < RichTextRenderer::BaseBlockRenderer
     def render(node)
-      hash = node['content'][0]['value'].hash
-      "<h2><a id='#{hash}'>#{render_content(node)}</a></h2>"
+      whole_heading = ''
+
+      node['content'].each do |content|
+        whole_heading += content['value']
+      end
+
+      hash = whole_heading.parameterize
+      "<h2><a id='#{hash}'>#{whole_heading}</a></h2>"
     end
   end
 end
